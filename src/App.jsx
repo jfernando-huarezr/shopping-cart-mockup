cc8a5470f11bafa2f44546be60e78a9dc3cd5c39
Jose Fernando Huarez Reyes <jose.huarezr@gmail.com>
2024-04-19 21:02:27 -0500

    create CartContext on App.jsx removed props

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,24 @@ import Header from './components/Header/Header'
 import Footer from './components/Footer/Footer'
 import MainBody from './components/MainBody/MainBody'
 import { useState } from 'react'
+import { createContext } from 'react'
+
+const CartContext = createContext({
+  cartCounter: 0,
+  setCartCounter: () => {},
+})
 
 function App() {
   const [cartCounter, setCartCounter] = useState(0)
 
   return (
-    <div className="flex min-h-screen flex-col justify-between">
-      <Header cartCounter={cartCounter} />
-      <MainBody setCartCounter={setCartCounter} />
-      <Footer />
-    </div>
+    <CartContext.Provider value={{ cartCounter, setCartCounter }}>
+      <div className="flex min-h-screen flex-col justify-between">
+        <Header />
+        <MainBody />
+        <Footer />
+      </div>
+    </CartContext.Provider>
   )
 }
 
